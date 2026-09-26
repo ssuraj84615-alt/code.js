4fdacc3a7166c15848a26b8174f127d8574a3b69
suraj kumar <ssuraj84615@gmail.com>
2026-09-26 20:56:35 +0530

    addng two numbers

--- code.js
+++ code.js
@@ -1,1 +1,7 @@
-console.log("hello.js");
+// How to add two numbers in JS .
+function addNumbers(num1, num2){
+    return num1 + num2
+}
+
+console.log( `The sum of 12 and 35 is = ${addNumbers(12, 35)}` );
+console.log( `The sum of 116 and 143 is = ${addNumbers(116, 143)}` );
