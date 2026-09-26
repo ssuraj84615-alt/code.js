35588576d5473b09d5fdb5334ea174f4248b4e52
suraj kumar <ssuraj84615@gmail.com>
2026-09-26 19:40:03 +0530

    code

--- code.js
+++ code.js
@@ -1,1 +1,10 @@
-console.log("hello.js");
+// check the given number is "even" & "odd".
+checkOddEven = (number) => {
+    if(number % 2 === 0)
+        return 'Number is Even'
+    else
+        return 'Number is Odd'
+
+}
+console.log(checkOddEven(2));
+console.log(checkOddEven(35));
